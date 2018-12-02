06e1df2e4bd2179e7aedfa2571054dd044705153
Doxing1111 <44939167+Doxing1111@users.noreply.github.com>
2018-12-02 22:13:20 +0800

    Update Selenium.py

--- Selenium.py
+++ Selenium.py
@@ -5,7 +5,7 @@ import time
 path = 'C:\Program Files (x86)\Python37-32\chromedriver.exe'
 
 b = webdriver.Chrome(path)
-b.get('http://wap.skylongtech.com/wap/dist/#/AppIndex')
+b.get('http://')
 time.sleep(2)
 b.set_window_size('414','736')
 
@@ -18,7 +18,7 @@ time.sleep(1)
 signin.click()
 
 UserID = b.find_elements_by_xpath('/html/body/div[1]/div[2]/ul/li[1]/input')
-UserID.send_keys('joeuse1010')
+UserID.send_keys('test')
 
 PSW = b.find_elements_by_xpath('/html/body/div[1]/div[2]/ul/li[2]/input')
 PSW.send_keys('a123456')
